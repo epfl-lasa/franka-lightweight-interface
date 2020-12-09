225a8c78fbf0a3c0b201ad7d0ff97159e415b3c5
Enrico Eberhard <enrico.eberhard@gmail.com>
2020-12-09 19:46:19 +0100

    Add Jacobian and reformat
    Add Jacobian array to com protocol (cartesian dof x joints)
    
    Fill Jacobian message in control loop
    
    Reformat code according to LASA style

--- include/franka_lightweight_interface/FrankaLightWeightInterface.hpp
+++ include/franka_lightweight_interface/FrankaLightWeightInterface.hpp
@@ -13,133 +13,126 @@
 
 #include "franka_lightweight_interface/franka_lwi_communication_protocol.h"
 
+namespace frankalwi {
+/**
+ * @class FrankaRobotInterface
+ * @brief Class to define an interface with the Franka panda robots
+ *
+ */
+class FrankaLightWeightInterface {
+private:
+  std::string robot_ip_; ///< ip of the robot to connect to
+  std::unique_ptr<franka::Robot> franka_robot_; ///< robot object to send command to
+  std::unique_ptr<franka::Model> franka_model_; ///< model object of the robot
+  bool connected_;
+  bool shutdown_;
+  zmq::context_t zmq_context_;
+  zmq::socket_t zmq_publisher_;
+  zmq::socket_t zmq_subscriber_;
+  proto::CommandMessage<7> zmq_command_msg_{};
+  proto::StateMessage<7> zmq_state_msg_{};
+  Eigen::Vector3d current_cartesian_position_;
+  Eigen::Quaterniond current_cartesian_orientation_;
+  Eigen::Matrix<double, 6, 1> current_cartesian_twist_;
+  Eigen::Matrix<double, 6, 1> current_cartesian_wrench_;
+  Eigen::Matrix<double, 7, 1> current_joint_positions_;
+  Eigen::Matrix<double, 7, 1> current_joint_velocities_;
+  Eigen::Matrix<double, 7, 1> current_joint_torques_;
+  Eigen::Matrix<double, 6, 7> current_jacobian_;
+  Eigen::Matrix<double, 7, 1> command_joint_torques_;
+  std::mutex mutex_;
 
-namespace frankalwi
-{
-    /**
-     * @class FrankaRobotInterface
-     * @brief Class to define an interface with the Franka panda robots
-     *
-     */
-    class FrankaLightWeightInterface
-    {
-    private:
-        std::string robot_ip_; ///< ip of the robot to connect to
-        std::unique_ptr<franka::Robot> franka_robot_; ///< robot object to send command to
-        std::unique_ptr<franka::Model> franka_model_; ///< model object of the robot
-        bool connected_;
-        bool shutdown_;
-        zmq::context_t zmq_context_;
-        zmq::socket_t zmq_publisher_;
-        zmq::socket_t zmq_subscriber_;
-        proto::CommandMessage<7> zmq_command_msg_;
-        proto::StateMessage<7> zmq_state_msg_;
-        Eigen::Vector3d current_cartesian_position_;
-        Eigen::Quaterniond current_cartesian_orientation_;
-        Eigen::Matrix<double, 6, 1> current_cartesian_twist_;
-        Eigen::Matrix<double, 6, 1> current_cartesian_wrench_;
-        Eigen::Matrix<double, 7, 1> current_joint_positions_;
-        Eigen::Matrix<double, 7, 1> current_joint_velocities_;
-        Eigen::Matrix<double, 7, 1> current_joint_torques_;
-        Eigen::Matrix<double, 6, 7> current_jacobian_;
-        Eigen::Matrix<double, 7, 1> command_joint_torques_;
-        std::mutex mutex_;
+  void print_state() const;
 
-        void print_state() const;
+public:
+  /**
+   * @brief Constructor for the FrankaLightWeightInterface class
+   * @param robot_ip ip adress of the robot to control
+   */
+  explicit FrankaLightWeightInterface(std::string robot_ip);
 
-    public:
-        /**
-         * @brief Constructor for the FrankaLightWeightInterface class
-         * @param robot_ip ip adress of the robot to control
-         */
-        explicit FrankaLightWeightInterface(const std::string & robot_ip);
+  /**
+   * @brief Getter of the connected boolean attribute
+   * @return the value of the connected attribute
+   */
+  bool is_connected() const;
 
-        /**
-         * @brief Getter of the connected boolean attribute
-         * @return the value of the connected attribute
-         */
-        bool is_connected() const;
+  /**
+   * @brief Getter of the shutdown boolean attribute
+   * @return the value of the shutdown attribute
+   */
+  bool is_shutdown() const;
 
-        /**
-         * @brief Getter of the shutdown boolean attribute
-         * @return the value of the shutdown attribute
-         */
-        bool is_shutdown() const;
+  /**
+   * Getter of the mutex attribute
+   * @return the mutex attribute
+   */
+  std::mutex& get_mutex();
 
-        /**
-         * Getter of the mutex attribute
-         * @return the mutex attribute
-         */
-        std::mutex& get_mutex();
+  /**
+   * @brief Initialize the connection to the robot
+   */
+  void init();
 
-        /**
-         * @brief Initialize the connection to the robot
-         */
-        void init();
+  /**
+   * @brief Threaded function that run a controller based on the value in the active_controller enumeration
+   */
+  void run_controller();
 
-        /**
-         * @brief Threaded function that run a controller based on the value in the active_controller enumeration
-         */
-        void run_controller();
+  /**
+   * @brief Poll the ZMQ socket subscription for a new joint torque command from an external controller
+   */
+  void poll_external_command();
 
-        /**
-         * @brief Poll the ZMQ socket subscription for a new joint torque command from an external controller
-         */
-        void poll_external_command();
+  /**
+  * @brief Publish robot state to the ZMQ socket for an external controller or observer to receive
+  */
+  void publish_robot_state();
 
-        /**
-        * @brief Publish robot state to the ZMQ socket for an external controller or observer to receive
-        */
-        void publish_robot_state();
+  /**
+   * @brief Read the robot state and update the published elements
+   * @param robot_state the Franka robot state to read and get velues from
+   */
+  void read_robot_state(const franka::RobotState& robot_state);
 
-        /**
-         * @brief Read the robot state and update the published elements
-         * @param robot_state the Franka robot state to read and get velues from
-         */
-        void read_robot_state(const franka::RobotState& robot_state);
+  /**
+   * @brief Run the joint torques controller
+   * that reads commands from the joint torques subscription
+   */
+  void run_joint_torques_controller();
 
-        /**
-         * @brief Run the joint torques controller
-         * that reads commands from the joint torques subscription
-         */
-        void run_joint_torques_controller();
+};
 
+inline bool FrankaLightWeightInterface::is_connected() const {
+  return this->connected_;
+}
 
-    };
+inline bool FrankaLightWeightInterface::is_shutdown() const {
+  return this->shutdown_;
+}
 
-    inline bool FrankaLightWeightInterface::is_connected() const
-    {
-        return this->connected_;
-    }
+inline std::mutex& FrankaLightWeightInterface::get_mutex() {
+  return this->mutex_;
+}
 
-    inline bool FrankaLightWeightInterface::is_shutdown() const
-    {
-        return this->shutdown_;
-    }
-
-    inline std::mutex& FrankaLightWeightInterface::get_mutex()
-    {
-        return this->mutex_;
-    }
-
-    inline void FrankaLightWeightInterface::print_state() const
-    {
-        std::cout << "Current robot cartesian state:" << std::endl;
-        std::cout << "--------------------" << std::endl;
-        std::cout << this->current_cartesian_position_.transpose() << std::endl;
-        std::cout << this->current_cartesian_orientation_.w() << ", " << this->current_cartesian_orientation_.vec().transpose() << std::endl;
-        std::cout << this->current_cartesian_twist_.transpose() << std::endl;
-        std::cout << this->current_cartesian_wrench_.transpose() << std::endl;
-        std::cout << "--------------------" << std::endl;
-        std::cout << "Current robot joint state:" << std::endl;
-        std::cout << "--------------------" << std::endl;
-        std::cout << this->current_joint_positions_.transpose() << std::endl;
-        std::cout << this->current_joint_velocities_.transpose() << std::endl;
-        std::cout << this->current_joint_torques_.transpose() << std::endl;
-        std::cout << "--------------------" << std::endl;
-        std::cout << "Commanded torque:" << std::endl;
-        std::cout << "--------------------" << std::endl;
-        std::cout << this->command_joint_torques_.transpose() << std::endl;
-        std::cout << "####################" << std::endl;
-    }
+inline void FrankaLightWeightInterface::print_state() const {
+  std::cout << "Current robot cartesian state:" << std::endl;
+  std::cout << "--------------------" << std::endl;
+  std::cout << this->current_cartesian_position_.transpose() << std::endl;
+  std::cout << this->current_cartesian_orientation_.w() << ", "
+            << this->current_cartesian_orientation_.vec().transpose() << std::endl;
+  std::cout << this->current_cartesian_twist_.transpose() << std::endl;
+  std::cout << this->current_cartesian_wrench_.transpose() << std::endl;
+  std::cout << "--------------------" << std::endl;
+  std::cout << "Current robot joint state:" << std::endl;
+  std::cout << "--------------------" << std::endl;
+  std::cout << this->current_joint_positions_.transpose() << std::endl;
+  std::cout << this->current_joint_velocities_.transpose() << std::endl;
+  std::cout << this->current_joint_torques_.transpose() << std::endl;
+  std::cout << "--------------------" << std::endl;
+  std::cout << "Commanded torque:" << std::endl;
+  std::cout << "--------------------" << std::endl;
+  std::cout << this->command_joint_torques_.transpose() << std::endl;
+  std::cout << "####################" << std::endl;
+}
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,11 @@
-//
-// Created by eeberhard on 24/11/2020.
-//
-
 #include "franka_lightweight_interface/FrankaLightWeightInterface.hpp"
 
 using namespace frankalwi;
 
-int main(int, char**)
-{
-    std::string robot_ip = "172.16.0.2";
-    FrankaLightWeightInterface flwi(robot_ip);
-    flwi.init();
-    flwi.run_controller();
-    return 0;
+int main(int, char**) {
+  std::string robot_ip = "172.16.0.2";
+  FrankaLightWeightInterface flwi(robot_ip);
+  flwi.init();
+  flwi.run_controller();
+  return 0;
 }
