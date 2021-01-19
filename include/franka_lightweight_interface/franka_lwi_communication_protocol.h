67cf06445266002c5116f89c2f42158b4e17b4f9
Enrico Eberhard <enrico.eberhard@gmail.com>
2021-01-19 15:40:14 +0100

    Refactor proto type definitions
    * Convert Jacobian and mass matrices to simple linear arrays,
    so that mapping to and from Eigen types is much simpler.
    
    * Use precision type double instead of float, to match the
    predominant usage of doubles in Eigen types.
    
    * Create explicit constructors for frankalwi::proto types
    Vec3D, Quaternion, EEPose and EETwist that can distribute
    elements of an array to the internal properties.
    
    * Use Eigen mapping in FrankaLightWeightInterface source file
    instead of nested loops and multiple single-assignment lines.

--- include/franka_lightweight_interface/franka_lwi_communication_protocol.h
+++ include/franka_lightweight_interface/franka_lwi_communication_protocol.h
@@ -8,19 +8,23 @@ namespace frankalwi::proto {
 // --- Message sub-structures --- //
 template<std::size_t DOF>
 struct Joints {
-  float& operator[](std::size_t i) {
+  double& operator[](std::size_t i) {
     return data[i % DOF];
   }
-  std::array<float, DOF> data;
+  std::array<double, DOF> data;
 };
 
 struct Vec3D {
+  Vec3D() : x(0), y(0), z(0) {}
+  explicit Vec3D(std::array<double, 3> vec) : x(vec[0]), y(vec[1]), z(vec[2]) {}
   float x;
   float y;
   float z;
 };
 
 struct Quaternion {
+  Quaternion() : w(0), x(0), y(0), z(0) {}
+  explicit Quaternion(std::array<double, 4> q) : w(q[0]), x(q[1]), y(q[2]), z(q[3]) {}
   float w;
   float x;
   float y;
@@ -28,20 +32,28 @@ struct Quaternion {
 };
 
 struct EEPose {
-  Vec3D position{};
-  Quaternion orientation{};
+  EEPose() : position(), orientation() {}
+  explicit EEPose(std::array<double, 7> pose) : position({pose[0], pose[1], pose[2]}),
+                                                orientation({pose[3], pose[4], pose[5], pose[6]}) {
+
+  }
+  Vec3D position;
+  Quaternion orientation;
 };
 
 struct EETwist {
-  Vec3D linear{};
-  Vec3D angular{};
+  EETwist() : linear(), angular() {}
+  explicit EETwist(std::array<double, 6> twist) : linear({twist[0], twist[1], twist[2]}),
+                                                  angular({twist[3], twist[4], twist[5]}) {}
+  Vec3D linear;
+  Vec3D angular;
 };
 
 template<std::size_t DOF>
-using Jacobian = std::array<Joints<DOF>, 6>;
+using Jacobian = std::array<double, 6 * DOF>;
 
 template<std::size_t DOF>
-using Mass = std::array<Joints<DOF>, DOF>;
+using Mass = std::array<double, DOF * DOF>;
 
 
 // --- Message structures --- //
