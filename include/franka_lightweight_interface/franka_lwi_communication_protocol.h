8b84debe8a589f82d43b284cebf5dd4d9d963044
Enrico Eberhard <enrico.eberhard@epfl.ch>
2021-01-20 10:05:05 +0100

    Add datatype typedef
    * Fix accidental instances of float in Vec3D and Quaternion
    
    * Redefine double using a typedef in the local namespace
    
    To ensure the datatype of the frankalwi proto is consistent
    and can be updated or checked easily, I have added a typedef
    of frankalwi::proto::datatype to represent the double type

--- include/franka_lightweight_interface/franka_lwi_communication_protocol.h
+++ include/franka_lightweight_interface/franka_lwi_communication_protocol.h
@@ -5,36 +5,38 @@
 
 namespace frankalwi::proto {
 
+typedef double datatype;
+
 // --- Message sub-structures --- //
 template<std::size_t DOF>
 struct Joints {
-  double& operator[](std::size_t i) {
+  datatype& operator[](std::size_t i) {
     return data[i % DOF];
   }
-  std::array<double, DOF> data;
+  std::array<datatype, DOF> data;
 };
 
 struct Vec3D {
   Vec3D() : x(0), y(0), z(0) {}
-  explicit Vec3D(std::array<double, 3> vec) : x(vec[0]), y(vec[1]), z(vec[2]) {}
-  float x;
-  float y;
-  float z;
+  explicit Vec3D(std::array<datatype, 3> vec) : x(vec[0]), y(vec[1]), z(vec[2]) {}
+  datatype x;
+  datatype y;
+  datatype z;
 };
 
 // Quaternion is stored with scalar term first, followed by real vector terms (w, x, y, z)
 struct Quaternion {
   Quaternion() : w(0), x(0), y(0), z(0) {}
-  explicit Quaternion(std::array<double, 4> q) : w(q[0]), x(q[1]), y(q[2]), z(q[3]) {}
-  float w;
-  float x;
-  float y;
-  float z;
+  explicit Quaternion(std::array<datatype, 4> q) : w(q[0]), x(q[1]), y(q[2]), z(q[3]) {}
+  datatype w;
+  datatype x;
+  datatype y;
+  datatype z;
 };
 
 struct EEPose {
   EEPose() : position(), orientation() {}
-  explicit EEPose(std::array<double, 7> pose) : position({pose[0], pose[1], pose[2]}),
+  explicit EEPose(std::array<datatype, 7> pose) : position({pose[0], pose[1], pose[2]}),
                                                 orientation({pose[3], pose[4], pose[5], pose[6]}) {
 
   }
@@ -44,7 +46,7 @@ struct EEPose {
 
 struct EETwist {
   EETwist() : linear(), angular() {}
-  explicit EETwist(std::array<double, 6> twist) : linear({twist[0], twist[1], twist[2]}),
+  explicit EETwist(std::array<datatype, 6> twist) : linear({twist[0], twist[1], twist[2]}),
                                                   angular({twist[3], twist[4], twist[5]}) {}
   Vec3D linear;
   Vec3D angular;
@@ -55,10 +57,10 @@ struct EETwist {
 // For a 6x7 matrix, the serial indices   0,      1,      2     ...  6,      7,      8 ...
 //   map to the row/column indices as:   [0][0], [1][0], [2][0] ... [0][1], [1][1], [2][1] ...
 template<std::size_t DOF>
-using Jacobian = std::array<double, 6 * DOF>;
+using Jacobian = std::array<datatype, 6 * DOF>;
 
 template<std::size_t DOF>
-using Mass = std::array<double, DOF * DOF>;
+using Mass = std::array<datatype, DOF * DOF>;
 
 
 // --- Message structures --- //
