3387e0ad2d70d1641f6235f08acf2f6756f2f314
Dominic Reber <dominic.reber@epfl.ch>
2021-09-28 11:40:06 +0200

    Update example controller

--- examples/joint_position_controller.cpp
+++ examples/joint_position_controller.cpp
@@ -1,28 +1,40 @@
-#include "frankalwi_proto/frankalwi_network.h"
+#include "network_interfaces/control_type.h"
+#include "network_interfaces/zmq/network.h"
 
-int main(int, char**) {
+int main(int argc, char** argv) {
+  std::string state_uri = "*:1601";
+  std::string command_uri = "*:1602";
+  if (argc == 2 && atof(argv[1]) != 16) {
+    if (atof(argv[1]) == 17) {
+      state_uri = "*:1701";
+      command_uri = "*:1702";
+    } else {
+      std::cerr << "This robot is unknown, choose either '16' (default) or '17'." << std::endl;
+      return 1;
+    }
+  } else {
+    std::cerr << "Please provide at one argument to choose which robot to connect to ('16' or '17')." << std::endl;
+    return 1;
+  }
+
   double gain = 10.0;
   double damping = 0.1;
 
-  zmq::context_t context(1);
-  zmq::socket_t subscriber;
-  zmq::socket_t publisher;
-  frankalwi::network::configure_sockets(context, subscriber, "0.0.0.0:1701", publisher, "0.0.0.0:1702");
+  ::zmq::context_t context(1);
+  ::zmq::socket_t subscriber, publisher;
+  network_interfaces::zmq::configure_sockets(context, subscriber, state_uri, publisher, command_uri, true, true);
 
-  frankalwi::proto::StateMessage state;
-  frankalwi::proto::CommandMessage command;
+  network_interfaces::zmq::StateMessage state;
+  network_interfaces::zmq::CommandMessage command;
 
-  command.control_type = frankalwi::proto::ControlType::JOINT_TORQUE;
-  command.ee_state = state_representation::CartesianState::Identity("franka_ee", "franka_base");
+  command.control_type = std::vector<int>{static_cast<int>(network_interfaces::control_type_t::EFFORT)};
   command.joint_state = state_representation::JointState::Zero("franka", 7);
 
   state_representation::JointPositions target;
   while (context.handle() != nullptr) {
-    if (frankalwi::network::poll(state, subscriber)) {
+    if (network_interfaces::zmq::receive(state, subscriber)) {
       if (target.is_empty()) {
         target = state.joint_state;
-        command.ee_state = state.ee_state;
-        command.ee_state.set_zero();
         command.joint_state = state.joint_state;
         command.joint_state.set_zero();
       }
@@ -32,7 +44,7 @@ int main(int, char**) {
       auto torques = gain * error - damping * state.joint_state.get_velocities();
       command.joint_state.set_torques(torques);
 
-      frankalwi::network::send(command, publisher);
+      network_interfaces::zmq::send(command, publisher);
     }
   }
 
