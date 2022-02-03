a0e9221213a14173d971771e2ba350b06e6448a2
Dominic Reber <dominic.reber@epfl.ch>
2022-02-03 15:26:22 +0100

    Use network interfaces image as base image

--- run-rt.sh
+++ run-rt.sh
@@ -2,19 +2,29 @@ IMAGE_NAME=epfl-lasa/franka-lightweight-interface
 IMAGE_TAG=runtime
 BUILD_FLAGS=()
 
-while getopts 'r' opt; do
-  case $opt in
-    r) BUILD_FLAGS+=(--no-cache) ;;
-    *) echo 'Error in command line parsing' >&2
-       exit 1
+HELP_MESSAGE="Usage: ./run-rt.sh
+Build and run a runtime docker container with realtime permissions.
+Options:
+  -r, --rebuild            Rebuild the image using the docker
+                           --no-cache option.
+  -v, --verbose            Use the verbose option during the building
+                           process.
+  -h, --help               Show this help message."
+
+BUILD_FLAGS=()
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    -r|--rebuild) BUILD_FLAGS+=(--no-cache); shift 1;;
+    -v|--verbose) BUILD_FLAGS+=(--progress=plain); shift 1;;
+    -h|--help) echo "${HELP_MESSAGE}"; exit 0;;
+    *) echo "Unknown option: $1" >&2; echo "${HELP_MESSAGE}"; exit 1;;
   esac
 done
-shift "$(( OPTIND - 1 ))"
 
 BUILD_FLAGS+=(--target "${IMAGE_TAG}")
 BUILD_FLAGS+=(-t "${IMAGE_NAME}":"${IMAGE_TAG}")
 
-docker pull ghcr.io/epfl-lasa/control-libraries/development-dependencies
+docker pull ghcr.io/aica-technology/network-interfaces
 DOCKER_BUILDKIT=1 docker build "${BUILD_FLAGS[@]}" . || exit 1
 
 docker run -it --rm --privileged --cap-add=SYS_NICE \
